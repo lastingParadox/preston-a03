a1dbb0c97404387aab57baf2b2c04879a8087035
lastingParadox <zanpres@gmail.com>
2021-09-25 10:28:56 -0400

    Added Solution24 and Anagram class skeletons

---
+++ exercise24/src/main/java/baseline/Anagram.java
@@ -0,0 +1,20 @@
+package baseline;
+
+public class Anagram {
+
+    public String sortString(String inputString) {
+        //Create a temporary array
+        //Set the array equal to the inputString using .toCharArray()
+        //Using the Arrays package, use the .sort() function on the array.
+        //Return the array as a new String.
+        return "null";
+    }
+
+    public boolean isAnagram(String firstSorted, String secondSorted) {
+        //If the firstSorted string is equal to the secondSorted string:
+        //Return true.
+        //Otherwise, return false.
+        return false;
+    }
+
+}
---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Zander Preston
+ */
+
+package baseline;
+
+public class Solution24 {
+
+    //Create new instance of Solution24 "reader"
+
+    public String takeInput(String position) {
+        //Prompt the user to input the "first" or "second" string using a Scanner input variable.
+        //Convert the string to lowercase.
+        //Return the string.
+        return "null";
+    }
+
+    public static void main(String[] args) {
+
+        //Create new instance of Anagram "sorter".
+        //Create string variables "firstString" and "secondString".
+
+        //Set "firstString" equal to takeInput("first").
+        //Set "secondString equal to takeInput("second).
+
+        //String "firstSorted" is equal to sorter's sortString(firstString).
+        //String "secondSorted" is equal to sorter's sortString(secondString).
+
+        //If sorter's isAnagram(firstSorted, secondSorted) is true:
+            //Output "\"firstString\" and "\secondString\" are anagrams."
+        //Else:
+            //Output "\"firstString\" and "\secondString\" are not anagrams."
+    }
+
+}
