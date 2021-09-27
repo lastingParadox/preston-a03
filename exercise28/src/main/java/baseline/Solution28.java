da2d4cded61c33dffc98fc5d2dd4c29cd232dee4
lastingParadox <zanpres@gmail.com>
2021-09-27 13:05:09 -0400

    Added exercise28 skeleton

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,44 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Zander Preston
+ */
+
+package baseline;
+
+public class Solution28 {
+
+    //Create final int loopNum = 5;
+
+    private int takeIntInput() {
+        //Prompt the user to enter a number.
+            //Store user's response and convert to an int.
+        //If the user does not enter a number:
+            //Prompt the user to enter a number.
+            //Store user's response.
+        return 0;
+    }
+
+    public int addTotal() {
+        //Create int total = 0
+        //Loop for loopNum times:
+            //total += takeIntInput()
+        //Return total
+        return 0;
+    }
+
+    public int addTotal(int[] array) {
+        //Overloading method for unit testing:
+            //Create int total = 0
+            //Loop for loopNum times:
+                //total += array[i]
+            //Return total
+        return 0;
+    }
+
+    public static void main(String[] arg) {
+        //Create an instance of Solution28 named "numCalculator"
+
+        //Integer total is equal to numCalculator.addTotal().
+        //Print "The total is 'total'."
+    }
+}
