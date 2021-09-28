40d2899fbea4ec4039624ab4ff1cdf51b93cb06a
lastingParadox <zanpres@gmail.com>
2021-09-28 09:11:30 -0400

    Created exercise30 skeleton

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Zander Preston
+ */
+
+package baseline;
+
+public class Solution30 {
+
+    //Create final int tableSize = 12
+
+    public int multiply(int num1, int num2) {
+        //Simply multiplies two numbers and returns their product.
+        //Used for easier comprehension in the multiplication table and unit testing.
+        return 0;
+    }
+
+    public String createMultiplicationTable () {
+        //Create int product
+        //Create Stringbuilder table
+        //Loop for values 1 to tableSize (i)
+            //Loop for values 1 to tableSize (j)
+                //Product is equal to multiply(i, j)
+                //Append formatted product with four spaces (%4d) to table
+            //Append the next line to table
+        //Return String.valueOf(table)
+        return "";
+    }
+
+    public static void main(String[] arg) {
+        //Create new instance Solution30 "printer"
+
+        //Output printer.createMultiplicationTable()
+    }
+}
