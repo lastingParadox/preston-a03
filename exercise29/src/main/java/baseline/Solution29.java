661d3e9b9628446e2b93bdd8733160c6d5d50c15
lastingParadox <zanpres@gmail.com>
2021-09-27 22:39:13 -0400

    Added exercise29 skeleton

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Zander Preston
+ */
+
+package baseline;
+
+public class Solution29 {
+
+    private String takeInput() {
+        //Print "What is the rate of return?"
+        //Return user's input.
+        return "";
+    }
+
+    public double validateIntResponse() {
+        //Sets string response equal to takeInput()
+        //Try to cast the string to a double "doubleResponse"
+            //While the user did not enter a number:
+                //Prompt the user to enter a numerical value.
+                //response is set equal to takeInput()
+            //If doubleResponse is 0 or less:
+                //Prompt the user to enter a numerical value higher than zero.
+                //response is set equal to takeInput()
+        //Return doubleResponse.
+        return 0;
+    }
+
+    public double calcDoubleTime (double rate) {
+        //Return 72/rate.
+        return 0;
+    }
+
+    public static void main(String[] arg) {
+        //Creates a new instance of Solution29 "calculator"
+
+        //double rate is equal to calculator.validateIntResponse()
+            //Stores user's input for calculating double rate
+
+        //Output "It will take 'calculator.calcDoubleTime(rate)' years to double your initial investment."
+    }
+}
