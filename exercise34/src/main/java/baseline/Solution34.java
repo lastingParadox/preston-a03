c4a30680e1b91d0a033d3bade1da2e40f3d95bcc
Zander <zanpres@gmail.com>
2021-09-30 11:14:13 -0400

    Added test cases and method logic to exercise34

--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -1,56 +1,71 @@
 package baseline;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Solution34 {
 
-    //Create new instance Scanner "input"
+    Scanner input = new Scanner(System.in);
 
-    public String takeCapitalizedInput() {
-        //String "response" Takes in the user's input in lowercase.
-        //Create a String "responseCapitalized"
-        //Add the capitalized first letter of "response" to "responseCapitalized"
-        //Loop for every character in "response"
-            //If the character is whitespace:
-                //Add the whitespace + the capitalized character afterwards to responseCapitalized
-                //Increase the loop by two
-            //Else:
-                //Add the current character to responseCapitalized
-                //Increase the loop
-        return "";
+    public String takeCapitalizedInput(String response) {
+        //Takes the user's input and capitalizes it.
+        //Appends characters to a StringBuilder and adds a capital letter for every space.
+        StringBuilder capitalized = new StringBuilder();
+        capitalized.append(Character.toUpperCase(response.charAt(0)));
+
+        for(int i = 1; i < response.length();) {
+            char ch = response.charAt(i);
+
+            if(ch == ' ') {
+                capitalized.append(' ');
+                capitalized.append(Character.toUpperCase(response.charAt(i+1)));
+                i += 2;
+            }
+            else {
+                capitalized.append(ch);
+                i++;
+            }
+        }
+        return String.valueOf(capitalized);
     }
 
     public List createList() {
-        //Create new string list "employees"
-        //Add to employees:
-            //John Smith
-            //Jackie Jackson
-            //Chris Jones
-            //Amanda Cullen
-            //Jeremy Goodwin
-        //return employees
-        return null;
+        //Creates the list of employees.
+        List<String> employees = new ArrayList<>();
+
+        employees.add("John Smith");
+        employees.add("Jackie Johnson");
+        employees.add("Chris Jones");
+        employees.add("Amanda Cullen");
+        employees.add("Jeremy Goodwin");
+
+        return employees;
+
     }
 
     private void printList(List employees) {
-        //Print "There are 'employees.size()' employees:"
-        //Loop for the number of employees (i)
-            //Print the employee's name on a new line
-                //i.e., println(employees.get(i))
-        //Print a new line
+        //Prints the list of employees, along with the number.
+        System.out.printf("There are %d employees:%n", employees.size());
+
+        for (Object employee : employees) System.out.println(employee);
+
+        System.out.println();
     }
 
     public List deleteEmployee(String response, List employees) {
-        //Create boolean deleted = false
-        //Loop for all of the employees
-            //If response is equal to employees.get(i):
-                //Remove the employee from the list
-                //deleted = true
-        //If deleted is false:
-            //Throw an IllegalArgumentException
-                //"Invalid name"
-        //return employees
-        return null;
+        //Deletes the employee based on the user's capitalized response.
+        boolean deleted = false;
+        for (int i = 0; i < employees.size(); i++) {
+            if (response.equals(employees.get(i))) {
+                employees.remove(employees.get(i));
+                deleted = true;
+            }
+        }
+        //If response isn't an employee name, throws an error message.
+        if (!deleted)
+            throw new IllegalArgumentException("Invalid name.");
+        return employees;
     }
 
     public static void main(String[] arg) {
@@ -60,7 +75,8 @@ public class Solution34 {
         //Print employees
 
         //Print "Enter an employee name to remove: "
-        //String response takes in the input, capitalized.
+        //String response takes in the input
+        //Response is capitalized.
 
         //The employee is deleted if the name was typed correctly. (employees = deleteEmployee(response, employees))
 
